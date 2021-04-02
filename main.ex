25745838bf23033474c2ac598fea9bf127d50321
Tasha Chin <tasha.s.chin@gmail.com>
2021-04-02 04:45:14 -0700

    Added 'Hello World' example.

--- main.ex
+++ main.ex
@@ -1,3 +1,5 @@
 # Checks how often the chars in Str appears.
 iex>"Elixir"|>String.graphemes()|>Enum.frequencies()%{"E" ==> 1, "i" ==> 2, "l" ==> 1, "r" ==> 1, "x" ==> 1}
 
+# Classic hello world example
+iex > IO.put("Hello, world!")
